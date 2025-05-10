6be0e89b10f7059d7ed31c7dad54ed041c01045d
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-10 17:53:03 -0500

    update

--- app/src/main/java/com/example/juegodecolores/GameFragment.kt
+++ app/src/main/java/com/example/juegodecolores/GameFragment.kt
@@ -1,7 +1,11 @@
 package com.example.juegodecolores
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class GameFragment : Fragment(R.layout.fragment_game) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
