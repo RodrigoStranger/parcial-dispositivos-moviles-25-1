13e4dee235fc7fdac6f36e9602d64ce63ff8e9e6
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-10 17:58:29 -0500

    update

--- app/src/main/java/com/example/juegodecolores/WelcomeFragment.kt
+++ app/src/main/java/com/example/juegodecolores/WelcomeFragment.kt
@@ -1,7 +1,11 @@
 package com.example.juegodecolores
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
