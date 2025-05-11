5dcf1040774f8ccbc911d93ecd598266dd562d1b
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-10 21:31:30 -0500

    musica de inicio al WelcomeFragment.kt

--- app/src/main/java/com/example/juegodecolores/WelcomeFragment.kt
+++ app/src/main/java/com/example/juegodecolores/WelcomeFragment.kt
@@ -3,9 +3,21 @@ package com.example.juegodecolores
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import android.media.MediaPlayer
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.musica_de_fondo_inicio)
+        mediaPlayer?.isLooping = true
+        mediaPlayer?.start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
